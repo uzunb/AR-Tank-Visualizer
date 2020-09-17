3d79b2c93c74f349dbc2e5ee125ff365179851a5
Batuhan UZUN <39219223+uzunb@users.noreply.github.com>
2020-09-17 17:55:57 +0300

    Implemented tank rotation function added.

---
+++ ARTankProject/Assets/rotate.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Security.Permissions;
+using UnityEngine;
+
+public class rotate : MonoBehaviour
+{
+    private float AngleX ;
+    private float AngleY ;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+            transform.Rotate(25.0f, 0.0f, 0.0f, Space.Self);        
+
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+            transform.Rotate(-25.0f, 0.0f, 0.0f, Space.Self);        
+
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+            transform.Rotate(0.0f, 25.0f, 0.0f, Space.Self);        
+
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+            transform.Rotate(0.0f, -25.0f, 0.0f, Space.Self);
+    }
+}
