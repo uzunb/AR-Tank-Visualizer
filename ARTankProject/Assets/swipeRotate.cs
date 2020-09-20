cea86499936ae215aa10ab1336543fa762c32b4d
Batuhan UZUN <39219223+uzunb@users.noreply.github.com>
2020-09-20 16:46:33 +0300

    Added tank rotation feature by swiping in Android version.

---
+++ ARTankProject/Assets/swipeRotate.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using UnityEngine;
+
+public class swipeRotate : MonoBehaviour
+{
+    private Touch touch;
+    private Vector2 touchPosition;
+    private Quaternion rotationY;
+    private float rotateSpeedModifier = 0.1f;
+
+    void Update()
+    {
+
+        if (Input.touchCount > 0)
+        {
+            touch = Input.GetTouch(0);
+            if(touch.phase == TouchPhase.Moved)
+            {
+                rotationY = Quaternion.Euler(0f, -touch.deltaPosition.x * rotateSpeedModifier, 0f);
+                transform.rotation = rotationY * transform.rotation;
+            }
+        }
+    }
+}
